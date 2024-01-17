1932e6f32b76271edd37b2262c3d686f4ade6063
pmichalski98 <dev.michalski98@gmail.com>
2024-01-17 22:10:16 +0100

    config updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image'
-import {Button} from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
-  )
+export default async function Page() {
+  return <main></main>;
 }
