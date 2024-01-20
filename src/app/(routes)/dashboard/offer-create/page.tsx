9ae6ecb960a7bdb74425bde78461e55a8ee554a3
KACPER SINILO <kacperosinilo@gmail.com>
2024-01-20 17:02:23 +0100

    feat(/dashboard/offer-create): add images previews

--- src/app/(routes)/dashboard/offer-create/page.tsx
+++ src/app/(routes)/dashboard/offer-create/page.tsx
@@ -2,7 +2,7 @@ import AddOfferForm from "./_components/add-offer-form"
 
 function AddOfferPage() {
    return (
-      <section className="flex h-full flex-col items-center justify-center">
+      <section className="flex h-full flex-col items-center py-10">
          <h1 className="text-2xl font-bold">Dodaj ofertę </h1>
          <AddOfferForm />
       </section>
