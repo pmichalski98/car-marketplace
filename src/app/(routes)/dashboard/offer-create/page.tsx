76272b22ad7548cba5d32d370b46d7d588f3b727
pmichalski98 <dev.michalski98@gmail.com>
2024-01-21 22:15:26 +0100

    Revert "fix: git conflicts"
    This reverts commit 2e38d4cf02d4b876bddf2a3000abd5bae3ab3779.

--- src/app/(routes)/dashboard/offer-create/page.tsx
+++ src/app/(routes)/dashboard/offer-create/page.tsx
@@ -2,8 +2,8 @@ import AddOfferForm from "./_components/add-offer-form"
 
 function AddOfferPage() {
    return (
-      <section className="flex flex-col items-center justify-center">
-         <h1 className="text-2xl font-bold">Dodaj ofertę</h1>
+      <section className="flex  flex-col items-center justify-center">
+         <h1 className="text-2xl font-bold">Dodaj ofertę </h1>
          <AddOfferForm />
       </section>
    )
