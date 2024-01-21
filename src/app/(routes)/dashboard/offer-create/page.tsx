4597627bef7857b64eac0ad1f2b9824070473389
Kacper <89151689+kasin-it@users.noreply.github.com>
2024-01-21 21:55:55 +0100

    Revert "6 handle form submit on server side"

--- src/app/(routes)/dashboard/offer-create/page.tsx
+++ src/app/(routes)/dashboard/offer-create/page.tsx
@@ -2,8 +2,8 @@ import AddOfferForm from "./_components/add-offer-form"
 
 function AddOfferPage() {
    return (
-      <section className="flex flex-col items-center justify-center">
-         <h1 className="text-2xl font-bold">Dodaj ofertę</h1>
+      <section className="flex h-full flex-col items-center py-10">
+         <h1 className="text-2xl font-bold">Dodaj ofertę </h1>
          <AddOfferForm />
       </section>
    )
