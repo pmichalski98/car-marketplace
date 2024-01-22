834c84a7f0833152f3a0afca7b7146e67fd45a39
KACPER SINILO <kacperosinilo@gmail.com>
2024-01-22 11:13:48 +0100

    fix: merge

--- src/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import {pgTable, text,} from "drizzle-orm/pg-core";
-
-export const users = pgTable("users", {
-    id: text("id").notNull().primaryKey(),
-    name: text("name"),
-    email: text("email").notNull(),
-    image: text("image"),
-});
